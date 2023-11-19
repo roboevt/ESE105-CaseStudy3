e37607c54c89379998d93bd6b3f7bd1f981b712b
Eric Todd <roboevt@gmail.com>
2023-11-18 18:42:20 -0600

    Added 4 plot figures for part 2

--- part2.m
+++ part2.m
@@ -6,21 +6,64 @@ load("lightField.mat")
 
 % Configuration
 title_size = 18;
+label_size = 16;
 
-% Vary sensor width (part b)
+% ----------Part 2----------
+
+% -----Vary sensor width (part b)-----
+
+% Animation form
 % for w=0:.001:.1
 %     img = rays2img(rays(1,:), rays(3,:), w, 200);
 %     imshow(img);
 % end
 
-% Vary pixel count (part c)
+% 4 figures form
+imgw1 = rays2img(rays(1,:), rays(3,:), 0.01, 200);
+imgw2 = rays2img(rays(1,:), rays(3,:), 0.02, 200);
+imgw3 = rays2img(rays(1,:), rays(3,:), .05, 200);
+imgw4 = rays2img(rays(1,:), rays(3,:), .1, 200);
+
+%Plots
+figure
+subplot(2,2,1), imshow(imgw1);
+title("Sensor Width: 0.01","FontSize",label_size)
+subplot(2,2,2), imshow(imgw2);
+title("Sensor Width: 0.02","FontSize",label_size)
+subplot(2,2,3), imshow(imgw3);
+title("Sensor Width: 0.05","FontSize",label_size)
+subplot(2,2,4), imshow(imgw4);
+title("Sensor Width: 0.1","FontSize",label_size)
+
+% -----Vary pixel count (part c)-----
+
+% Animation form
 % figure;
 % for p=50:1000
 %     img = rays2img(rays(1,:), rays(3,:), .01, p);
 %     imshow(img);
 % end
 
-% Vary distance (part d)
+% 4 figures form
+imgp1 = rays2img(rays(1,:), rays(3,:), .01, 10);
+imgp2 = rays2img(rays(1,:), rays(3,:), .01, 50);
+imgp3 = rays2img(rays(1,:), rays(3,:), .01, 100);
+imgp4 = rays2img(rays(1,:), rays(3,:), .01, 500);
+
+%Plots
+figure
+subplot(2,2,1), imshow(imgp1);
+title("Pixels: 10","FontSize",label_size)
+subplot(2,2,2), imshow(imgp2);
+title("Pixels: 50","FontSize",label_size)
+subplot(2,2,3), imshow(imgp3);
+title("Pixels: 100","FontSize",label_size)
+subplot(2,2,4), imshow(imgp4);
+title("Pixels: 500","FontSize",label_size)
+
+% -----Vary distance (part d)-----
+
+% Animation form
 % for d = 0:.1:10
 %     Md = [1 d 0 0; 0 1 0 0; 0 0 1 d; 0 0 0 1];
 %     rays2 = Md*rays;
@@ -28,22 +71,32 @@ title_size = 18;
 %     imshow(img);
 % end
 
+% 4 figures form
+figure
+tiledlayout(2,2)
+i=1;
+for d = [0 .1 1 10]
+    Md = [1 d 0 0; 0 1 0 0; 0 0 1 d; 0 0 0 1];
+    rays2 = Md*rays; % Free space propagation
+    img = rays2img(rays2(1,:), rays2(3,:), .01, 500); % Capture image
+    subplot(2,2,i), imshow(img); %Plot
+    title("Distance: " + d) %Label
+    i = i+1;
+end
+
 % Plot best image so far
 d = 0.001;
 Md = [1 d 0 0; 0 1 0 0; 0 0 1 d; 0 0 0 1];
 rays2 = Md*rays;
 
+figure
 img = rays2img(rays2(1,:), rays2(3,:), .02, 500);
 imshow(img);
 title( "Raw Light Field", "FontSize", title_size)
 
-% Question 2:
-% We have no optical system. Light coming from one point of an object will
-% hit many points of the sensor...
-
 % Optical system design:
 
-% Used for focusing system (finding d1)
+% Used for focusing system (finding d1) Animation form
 % for d1=.3:.0015:.5 % Unknown distance from object to lens
 %     %d1 = .4;
 %     d2 = .8; % lens to sensor
@@ -63,6 +116,31 @@ title( "Raw Light Field", "FontSize", title_size)
 %     imshow(img)
 % end
 
+% 4 Figures form
+figure
+i = 1;
+for d1 = [.35, .38, .4, .42]
+    d2 = 1; % lens to sensor
+    f = (d1*d2)/(d1+d2);
+    
+    % Lens transformation
+    Mf = [1 0 0 0; -1/f 1 0 0; 0 0 1 0; 0 0 -1/f 1];
+
+    % Free space transformation between lens and sensor
+    Md2 = [1 d2 0 0; 0 1 0 0; 0 0 1 d2; 0 0 0 1];
+
+    % Transform rays to sensor
+    rays_focused = Md2*Mf*rays;
+
+    % Plot results
+    img = rays2img(rays_focused(1,:), rays_focused(3,:), .02, 1000);
+    subplot(2,2,i), imshow(img);
+    title("Focus Distance: " + d1 + "m", "FontSize",label_size);
+    i = i+1;
+end
+
+
+% ----------Part 3----------
 % Seperate image sources (imagine blocking part of the lens with a card)
 rays_lower_quarter = min(rays(1,:)) + range(rays(1,:))/3;
 rays_upper_quarter = min(rays(1,:)) + range(rays(1,:))*2/3;
